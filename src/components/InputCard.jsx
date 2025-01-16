79e31ab71cc9f665bf9a1a3eec7464e57bb85a3a
CoderGUY47 <shkhridoy92@gmail.com>
2025-01-16 23:51:25 +0600

    commit

--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import Flex from './Flex'
-import { SlLocationPin } from "react-icons/sl";
+import React from 'react';
+import Flex from './Flex';
 import { IoIosArrowDown } from "react-icons/io";
-
-const InputCard = () => {
+const InputCard = ({ icon, label, placeholder }) => {
   return (
     <div>
-        <Flex className='gap-x-3 items-center'>
-            <SlLocationPin className='text-secondary'/>
-            <p className='text-base text-nunito font-bold font-nunito text-bannerCard'>Location</p>
-            <IoIosArrowDown className='text-secondary'/>
-        </Flex>
-        <p className='text-paraText text-base text-nunito font-nunito font-medium pl-[27px]'>Enter Your Location</p>
-        {/* <input type="text" className="text-paraText text-base text-nunito font-nunito font-medium pl-[27px]" placeholder='Enter Your Location'/> */}
+      <Flex className='gap-x-3 items-center'>
+        {icon}
+        <p className='text-base text-nunito font-bold font-nunito text-bannerCard'>{label}</p>
+        <IoIosArrowDown className='text-secondary'/>
+      </Flex>
+      <p className='text-paraText text-base text-nunito font-nunito font-medium pl-[27px]'>{placeholder}</p>
     </div>
-  )
-}
+  );
+};
+export default InputCard;
 
-export default InputCard
+
+
+// import React from 'react';
+// import Flex from './Flex';
+// import { SlLocationPin } from "react-icons/sl";
+// import { IoIosArrowDown } from "react-icons/io";
+
+
+// const InputCard = ({icon, label, placeholder }) => {
+//   return (
+//     <div>
+//       <Flex className='gap-x-3 items-center'>
+//         <SlLocationPin className='text-secondary'/>
+//         <p className='text-base text-nunito font-bold font-nunito text-bannerCard'>{label}</p>
+//         <IoIosArrowDown className='text-secondary'/>
+//       </Flex>
+//       <p className='text-paraText text-base text-nunito font-nunito font-medium pl-[27px]'>{placeholder}</p>
+//     </div>
+//   );
+// };
+
+// export default InputCard;
--- src/layouts/Banner.jsx
+++ src/layouts/Banner.jsx
@@ -4,35 +4,39 @@ import Flex from '../components/Flex';
 import Button from '../components/Button';
 import InputCard from '../components/InputCard';
 import { FiSearch } from "react-icons/fi";
+import { SlLocationPin } from "react-icons/sl";
+import { HiOutlineCalendarDateRange } from "react-icons/hi2";
+import { IoPersonOutline } from "react-icons/io5";
 
 const Banner = () => {
   return (
     <section className='bg-banner'>
-        <Container>
-          <Flex>
-            <div className="w-1/2 pt-[27px] pb-[100px]">
-              <h1 className='pt-10 pb-2 w-[590px] text-[65px] text-primary font-semibold font-poppins leading-[70px]'>
-                Start Exploring to Find Inner Peace by Traveling.</h1>
-              <p className="pt-5 pb-7 w-[590px] text-base text-third font-normal font-poppins text-paraText leading-7">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis commodi amet est. 
-                Incidunt officia vero beatae est, nostrum similique veniam?
-              </p>
-              <Button text='Discover Now'/>
-              <div className="relative w-[825px] mt-10 bg-white rounded-lg rounded-tl-none pb-0">
-                <Flex className='py-9 px-6 gap-[50px]'>
-                  <InputCard/>
-                  <InputCard/>
-                  <InputCard/>
-                </Flex>
-                <button className='absolute top-5 right-7 p-[30px] bg-secondary rounded-lg'>
-                  <FiSearch className='text-white text-xl'/>
-                </button>
-              </div>
+      <Container>
+        <Flex>
+          <div className="w-1/2 pt-[27px] pb-[100px]">
+            <h1 className='pt-10 pb-2 w-[590px] text-[65px] text-primary font-semibold font-poppins leading-[70px]'>
+              Start Exploring to Find Inner Peace by Traveling.</h1>
+            <p className="pt-5 pb-7 w-[590px] text-base text-third font-normal font-poppins text-paraText leading-7">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis commodi amet est. 
+              Incidunt officia vero beatae est, nostrum similique veniam?
+            </p>
+            <Button text='Discover Now'/>
+            <div className="relative w-[825px] mt-10 bg-white rounded-lg rounded-tl-none pb-0">
+              <Flex className='py-9 px-6 gap-[50px]'>
+                <InputCard icon={<SlLocationPin/>} label="Destination" placeholder="Enter destination" />
+                <InputCard icon={<HiOutlineCalendarDateRange />} label="Date" placeholder="Select date" />
+                <InputCard icon={<IoPersonOutline />} label="User" placeholder="Select date" />
+              </Flex>
+              <button className='absolute top-5 right-7 p-[30px] bg-secondary rounded-lg'>
+                <FiSearch className='text-white text-xl'/>
+              </button>
             </div>
-            <div className="w-1/2"></div>
-          </Flex>
-        </Container>
+          </div>
+          <div className="w-1/2"></div>
+        </Flex>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
